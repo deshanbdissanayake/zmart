ea1ffb93bd561414b937040d1be54beaecbcac04
deshintrops <desh.introps@gmail.com>
2023-06-30 09:44:14 +0530

    20230630-0944

--- screens/OrderListScreen.js
+++ screens/OrderListScreen.js
@@ -1,39 +1,45 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, FlatList, ActivityIndicator, SafeAreaView, TouchableOpacity } from 'react-native';
-import { Ionicons, MaterialCommunityIcons } from 'react-native-vector-icons';
+import { View, Text, StyleSheet, FlatList, ActivityIndicator, SafeAreaView, TouchableOpacity, RefreshControl } from 'react-native';
+import { MaterialCommunityIcons } from 'react-native-vector-icons';
+
+import { useNavigation } from '@react-navigation/native';
 
 import colors from '../assets/colors/colors';
 import Footer from '../components/footer/Footer';
 import { getMyOrders } from '../assets/data/order';
 
 const OrderItem = ({ order }) => {
-    const handleItemPress = () => {
-      console.log('orderData', order);
-    };
-  
-    return (
-      <TouchableOpacity onPress={handleItemPress}>
-        <View style={styles.orderItemWrapper}>
-          <Text style={styles.orderItemId}>{order.ord_id.toString().padStart(3, '0')}</Text>
-          <View style={styles.orderItemTitleWrapper}>
-            <Text style={styles.orderItemTitle} numberOfLines={1}>Buyeasdfsdfr Nams</Text>
-            <Text style={styles.orderItemPhone} numberOfLines={1}>0714578456</Text>
-            <Text style={styles.orderItemSubTitle}>2023-06-28 13:30:00</Text>
-          </View>
-          <MaterialCommunityIcons
-                style={styles.iconStyles}
-                name="chevron-double-right"
-                size={24}
-                color="black"
-              />
-        </View>
-      </TouchableOpacity>
-    );
+  const navigation = useNavigation();
+
+  const handleItemPress = () => {
+    navigation.navigate('Single Order', { order })
+    //console.log('orderData', order);
   };
-  
+
+  return (
+    <TouchableOpacity onPress={handleItemPress}>
+      <View style={styles.orderItemWrapper}>
+        <Text style={styles.orderItemId}>{order.ord_id.toString().padStart(3, '0')}</Text>
+        <View style={styles.orderItemTitleWrapper}>
+          <Text style={styles.orderItemTitle} numberOfLines={1}>{order.buyer_name}</Text>
+          <Text style={styles.orderItemPhone} numberOfLines={1}>{order.buyer_phone}</Text>
+          <Text style={styles.orderItemSubTitle}>{order.c_date}</Text>
+        </View>
+        <MaterialCommunityIcons
+          style={styles.iconStyles}
+          name="chevron-double-right"
+          size={24}
+          color="black"
+        />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
 const OrderListScreen = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [orders, setOrders] = useState([]);
+  const [isRefreshing, setIsRefreshing] = useState(false);
 
   useEffect(() => {
     fetchOrders();
@@ -48,7 +54,13 @@ const OrderListScreen = () => {
       console.log('Error fetching orders:', error);
     } finally {
       setIsLoading(false);
+      setIsRefreshing(false);
     }
+  };
+
+  const handleRefresh = () => {
+    setIsRefreshing(true);
+    fetchOrders();
   };
 
   const renderLoadingIndicator = () => (
@@ -66,6 +78,14 @@ const OrderListScreen = () => {
         data={orders}
         renderItem={renderItem}
         keyExtractor={(item) => item.ord_id.toString()}
+        refreshControl={
+          <RefreshControl
+            refreshing={isRefreshing}
+            onRefresh={handleRefresh}
+            colors={[colors.primary]}
+            progressBackgroundColor={colors.background}
+          />
+        }
       />
     </View>
   );
@@ -78,60 +98,56 @@ const OrderListScreen = () => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16,
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 16,
-    },
-    loadingContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    orderItemWrapper: {
-        paddingHorizontal: 10,
-        paddingVertical: 10,
-        borderWidth: 1,
-        borderColor: colors.border,
-        marginBottom: 8,
-        borderRadius: 8,
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    },
-    orderItemId: {
-        flex: 2,
-        marginRight: 10,
-        backgroundColor: colors.secondary,
-        paddingVertical: 15,
-        paddingHorizontal: 10,
-        borderRadius: 5,
-        width: '100%',
-        textAlign: 'center',
-    },
-    orderItemTitleWrapper:{
-        flex: 9,
-        marginRight: 10,
-    },
-    orderItemTitle:{
-
-    },
-    orderItemPhone: {
-
-    },
-    orderItemSubTitle:{
-        fontSize: 11,
-        color: colors.gray,
-    },
-    iconStyles: {
-        flex: 1,
-    },
-    
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  orderItemWrapper: {
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderWidth: 1,
+    borderColor: colors.border,
+    marginBottom: 8,
+    borderRadius: 8,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  orderItemId: {
+    flex: 2,
+    marginRight: 10,
+    backgroundColor: colors.secondary,
+    paddingVertical: 16,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    width: '100%',
+    textAlign: 'center',
+  },
+  orderItemTitleWrapper: {
+    flex: 9,
+    marginRight: 10,
+  },
+  orderItemTitle: {
+    fontWeight: 'bold',
+  },
+  orderItemSubTitle: {
+    fontSize: 11,
+    color: colors.gray,
+  },
+  iconStyles: {
+    flex: 1,
+  },
 });
 
 export default OrderListScreen;
--- screens/SingleOrderScreen.js
+++ screens/SingleOrderScreen.js
@@ -1,19 +1,68 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList, TextInput, Image } from 'react-native';
+import { AntDesign, Feather, Ionicons } from 'react-native-vector-icons';
+import colors from '../assets/colors/colors';
+
+const Item = ({ orderData }) => {
+  console.log(orderData)
+  return (
+    <View style={styles.itemCardWrapper}>
+        <Image source={orderData.pro_img} style={styles.itemCardImgStyles} />
+      <View style={styles.itemCardTextStyles}>
+        <Text style={styles.itemCardNameStyles}>{orderData.pro_name}</Text>
+        <Text style={styles.itemCardPriceStyles}>LKR {(parseFloat(orderData.ord_price)).toFixed(2)}</Text>
+      </View>
+      <View style={styles.itemCardQtyStyles}>
+        <AntDesign name="caretup" size={20} color="black" />
+        <TextInput
+          value={(parseInt(orderData.ord_qty)).toString()}
+          onChange={() => {}}
+          style={styles.inputStyle}
+        />
+        <AntDesign name="caretdown" size={20} color="black" />
+      </View>
+    </View>
+  );
+};
 
 const SingleOrderScreen = ({ route }) => {
-  const { orderId, orderName } = route.params;
+  const { order } = route.params;
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Order Details</Text>
-      <View style={styles.orderItem}>
-        <Text style={styles.label}>Order ID:</Text>
-        <Text style={styles.value}>{orderId}</Text>
-      </View>
-      <View style={styles.orderItem}>
-        <Text style={styles.label}>Order Name:</Text>
-        <Text style={styles.value}>{orderName}</Text>
+      <View style={styles.orderWrapper}>
+        <View style={styles.orderDetailsWrapper}>
+          <Text style={styles.titleStyles}>Order Details</Text>
+          <View style={styles.orderDetailsStyles}>
+            <View style={styles.odTopStyles}>
+              <Text style={styles.orderIdStyle}>Order ID : #0001</Text>
+              <Text style={styles.orderDateStyle}>2023-06-29 12:45:00</Text>
+            </View>
+            <View style={styles.odMiddleStyles}>
+              <Text>Buyer Name - Buyer Shop</Text>
+            </View>
+            <View style={styles.odBottomStyles}>
+              <TouchableOpacity style={styles.contactButtonStyles} onPress={() => console.log('Call')}>
+                <View style={styles.iconStyles}><Feather name="phone-call" size={20} color="black" /></View>
+                <Text style={styles.orderNumberStyle}>0714124766</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.contactButtonStyles} onPress={() => console.log('WhatsApp')}>
+                <View style={styles.iconStyles}><Ionicons name="logo-whatsapp" size={20} color="black" /></View>
+                <Text style={styles.orderNumberStyle}>0714124766</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <View>
+          <Text style={styles.titleStyles}>Order Items</Text>
+          <View style={styles.orderItemsStyles}>
+            <FlatList
+              data={order.order_items}
+              renderItem={({ item }) => <Item orderData={item}/>}
+              keyExtractor={item => item.ordi_id}
+            />
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -22,23 +71,92 @@ const SingleOrderScreen = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
+    padding: 20,
   },
-  title: {
+  orderWrapper: {
+    flex: 1,
+  },
+  titleStyles: {
     fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 16,
+    marginBottom: 10,
   },
-  orderItem: {
+  orderDetailsWrapper: {
+    marginBottom: 15,
+  },
+  orderDetailsStyles: {
+    borderWidth: 1,
+    borderColor: colors.border,
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+  },
+  odTopStyles: {
     flexDirection: 'row',
-    marginBottom: 8,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
   },
-  label: {
+  orderIdStyle: {
     fontWeight: 'bold',
-    marginRight: 8,
   },
-  value: {
+  orderDateStyle: {
+    fontSize: 11,
+    color: colors.gray,
+  },
+  odMiddleStyles: {
+    marginBottom: 10,
+  },
+  odBottomStyles: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  contactButtonStyles: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  iconStyles: {
+    backgroundColor: colors.secondary,
+    padding: 5,
+    marginRight: 5,
+    borderRadius: 5,
+  },
+  orderNumberStyle: {
+    textDecorationLine: 'underline',
+  },
+  itemCardWrapper: {
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: colors.border,
+    flexDirection: 'row',
+  },
+  itemCardImgStyles: {
+    flex: 3,
+    backgroundColor: colors.gray,
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+    borderRadius: 10,
+    marginRight: 10,
+  },
+  itemCardTextStyles: {
+    flex: 8,
+  },
+  itemCardNameStyles: {
+    
+  },
+  itemCardPriceStyles: {
+    
+  },
+  itemCardQtyStyles: {
     flex: 1,
+  },
+  inputStyle: {
+    
   },
 });
 
