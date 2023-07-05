638fbcc7bf2bcdb1a946c74c6b955b693c6c4dc7
deshintrops <desh.introps@gmail.com>
2023-07-05 11:12:52 +0530

    20230705-1112

--- assets/data/order.js
+++ assets/data/order.js
@@ -1,3 +1,13 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const getLogData = async () => {
+  const data = await AsyncStorage.getItem('log_data');
+  if (!data) {
+    throw new Error('log_data does not exist in AsyncStorage (user.js)');
+  }
+  return JSON.parse(data);
+};
+
 const getMyOrders = async () => {
     const orders = [
       {
@@ -97,7 +107,61 @@ const getMyOrders = async () => {
     ];
   
     return orders;
-  };
+};
+
+const getMyOr = async () => {
+  try {
+    const logData = await getLogData();
+    const { log_userToken, log_userNumber, log_userType } = logData;
+    const url = 'https://v2.genzo.lk/App_api/getSupplierOrders';
+
+    const headers = {
+      userToken: log_userToken,
+      userPhone: log_userNumber,
+      userType: log_userType,
+    };
+
+    const options = {
+      method: 'GET',
+      headers: headers,
+    };
+
+    const response = await fetch(url, options);
+    const responseData = await response.json();
+    return responseData;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error occurred while getting products');
+  }
+};
+
+const confirmOrder = async (formData) => {
+  try {
+    const logData = await getLogData();
+    const { log_userToken, log_userNumber, log_userType } = logData;
+    const url = 'https://v2.genzo.lk/App_api/confirmOrder';
+
+    const headers = {
+      'Content-Type': 'multipart/form-data',
+      userToken: log_userToken,
+      userPhone: log_userNumber,
+      userType: log_userType,
+    };
+
+    const options = {
+      method: 'POST',
+      headers: headers,
+      body: formData,
+    };
+
+    const response = await fetch(url, options);
+    const responseData = await response.json();
+    return responseData;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error occurred while adding a product');
+  }
+}
   
   export { getMyOrders };
   
